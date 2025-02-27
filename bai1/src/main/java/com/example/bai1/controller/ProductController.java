9dbb75559e690807703857e57f3d6016488549bb
quang1606 <quangnguyenxuanymnb2004@gmail.com>
2025-02-27 13:46:23 +0700

    up

---
+++ bai1/src/main/java/com/example/bai1/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.example.bai1.controller;
+
+import com.example.bai1.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+public class ProductController {
+    private List<Product> products = new ArrayList<>(List.of(new Product("1","iphone 12","ngon",1222,"apple"),
+            new Product("2","iphone 13","ngon",1222,"apple"),
+            new Product("3","iphone 14","ngon",1220,"apple"),
+            new Product("4","iphone 15","ngon",1228,"apple"),
+            new Product("5","iphone 16","ngon",1227,"apple"),
+            new Product("6","iphone 17","ngon",1226,"apple"),
+            new Product("7","iphone 18","ngon",1224,"apple")));
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        return products;
+    }
+}
---
+++ bai1/src/main/java/com/example/bai1/model/Product.java
@@ -0,0 +1,16 @@
+package com.example.bai1.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private int price;
+    private String brand;
+}
