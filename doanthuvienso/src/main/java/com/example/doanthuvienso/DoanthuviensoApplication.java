93d31e09765e734d76c1b28cce73a03b80a8c1a2
quang1606 <quangnguyenxuanymnb2004@gmail.com>
2025-05-05 16:56:54 +0700

    up

---
+++ doanthuvienso/src/main/java/com/example/doanthuvienso/DoanthuviensoApplication.java
@@ -0,0 +1,13 @@
+package com.example.doanthuvienso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoanthuviensoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoanthuviensoApplication.class, args);
+    }
+
+}
